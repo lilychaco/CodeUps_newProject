363a2c789c7146f63ee78d3c01ae6b7f413cc677
lilychaco <lilychaco@gmail.com>
2023-11-25 17:15:41 +0900

    プライス、ボイス、インフォ、アバウトアス、campaignまで一応できた

--- src/js/script.js
+++ src/js/script.js
@@ -27,17 +27,39 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     $("#js-global-menu").css("visibility", "hidden");
     $("#js-global-menu").attr("aria-hidden", "true");
     $("#js-drawer-background").removeClass("is-drawerActive"); //ドロワーからis-activeクラスを外す
+	});
+
+
+  //campaignのスライダー用
+  let campaignSlider = new Swiper(".js-campaign-swiper", {
+		loop: true,
+    autoplay: {
+      delay: 2000,
+    },
+		speed: 2000,
+		slidesPerView: 'auto', // スライドの数を自動調整
+    breakpoints: {
+        769: { // 769px以上のデバイスでの設定
+            slidesPerView: 3.5, // PCでは3枚半表示
+        }
+    }
   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
-
-
-
-
-
-
-
-
-
-
-
-
