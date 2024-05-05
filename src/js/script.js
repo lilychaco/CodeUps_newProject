189a608b33733f8e9f966b4bfb10b5270d7ec910
Yumi Nakamura <lilychaco@gmail.com>
2024-05-05 17:29:34 +0900

    script.js閉じタグ修正

--- src/js/script.js
+++ src/js/script.js
@@ -12,6 +12,9 @@ jQuery(function ($) {
 				.attr("aria-hidden", isExpanded);
 		});
 
+
+
+
 		// ドロワーのリンクがクリックされたらメニューを閉じる
 		$("#js-global-menu a").click(function () {
 			$("body").toggleClass("is-drawerActive");
@@ -22,11 +25,17 @@ jQuery(function ($) {
 			$("#js-drawer-background").removeClass("is-drawerActive");
 		});
 
+
+
+
 		// ボタンをクリックしてページトップに戻る
 		$(".js-page-top-button").click(function () {
 			$("body,html").animate({ scrollTop: 0 }, 1000, "swing");
 			return false;
 		});
+
+
+
 
 		// タブメニューの初期設定
 		$(".js-content:first-of-type").show();
@@ -37,6 +46,9 @@ jQuery(function ($) {
 			$(".js-content").hide().eq(index).fadeIn(300);
 		});
 
+
+
+
 		// アーカイブメニューの動作
 		$(".js-year-toggle").click(function () {
 			var $monthList = $(this).next(".side-archive__month-list");
@@ -45,11 +57,17 @@ jQuery(function ($) {
 			$(this).parent(".side-archive__year").toggleClass("active");
 		});
 
+
+
+
 		// アコーディオンの動作
 		$(".js-accordion-top").click(function () {
 			$(this).next().slideToggle(300);
 			$(this).toggleClass("is-close");
 		});
+
+
+
 
 		// モーダルの開閉
 		$(".js-modal-open").click(function () {
@@ -69,6 +87,9 @@ jQuery(function ($) {
 			$("body").css("overflow", "auto"); // スクロールを許可
 			return false;
 		});
+
+
+
 
 		// フォーム送信時の検証
 		$("form").submit(function (e) {
@@ -107,83 +128,90 @@ jQuery(function ($) {
 				window.location.href = "page-error.html";
 			}
 		});
-	});
-
-	// 画面の高さを取得してCSS変数として設定
-	function setVh() {
-		var vh = window.innerHeight * 0.01;
-		document.documentElement.style.setProperty("--vh", `${vh}px`);
-	}
-	// ページ読み込み時とウィンドウサイズ変更時に実行
-	window.addEventListener("load", setVh);
-	window.addEventListener("resize", setVh);
 
 
-	//FVのスライダー用
-	let fvSlider = new Swiper(".js-fv-swiper", {
-		slidesPerView: 1,
-		spaceBetween: 0,
-		effect: "fade", //フェード切り替え
-		autoplay: {
-			delay: 4000, //4秒後に次のスライドへ
-			disableOnInteraction: false, //ユーザーが操作しても自動再生を継続
-		},
-		speed: 2000,
-	});
 
-	//campaignのスライダー用
-	let campaignSlider = new Swiper(".js-campaign-swiper", {
-		loop: true,
-		autoplay: {
-			delay: 2000,
-			disableOnInteraction: false,
-		},
-		speed: 2000,
-		slidesPerView: "auto", // スライドの数を自動調整
-		// 前後の矢印
-		navigation: {
-			nextEl: ".custom-swiper-button-next",
-			prevEl: ".custom-swiper-button-prev",
-		},
 
-		breakpoints: {
-			769: {
-				// 769px以上のデバイスでの設定
-				slidesPerView: "auto", // PCでは3枚半表示
+		// 画面の高さを取得してCSS変数として設定
+		function setVh() {
+			var vh = window.innerHeight * 0.01;
+			document.documentElement.style.setProperty("--vh", `${vh}px`);
+		}
+		// ページ読み込み時とウィンドウサイズ変更時に実行
+		window.addEventListener("load", setVh);
+		window.addEventListener("resize", setVh);
+
+
+
+
+		//FVのスライダー用
+		let fvSlider = new Swiper(".js-fv-swiper", {
+			slidesPerView: 1,
+			spaceBetween: 0,
+			effect: "fade", //フェード切り替え
+			autoplay: {
+				delay: 4000, //4秒後に次のスライドへ
+				disableOnInteraction: false, //ユーザーが操作しても自動再生を継続
 			},
-		},
-	});
-
-
-	//画像に色背景がついてから、写真が出てくる
-	//要素の取得とスピードの設定
-	var box = $(".colorbox"),
-		speed = 700;
-
-	//.colorboxの付いた全ての要素に対して下記の処理を行う
-	box.each(function () {
-		$(this).append('<div class="color"></div>');
-		var color = $(this).find($(".color")),
-			image = $(this).find("img");
-		var counter = 0;
-		image.css('opacity', '0');
-		color.css('width', '0%');
-
-		//inviewを使って背景色が画面に現れたら処理をする
-		color.on('inview', function () {
-			if (counter == 0) {
-				$(this).delay(200).animate({ 'width': '100%' }, speed, function () {
-					image.css('opacity', '1');
-					$(this).css({ 'left': '0', 'right': 'auto' });
-					$(this).animate({ 'width': '0%' }, speed);
-				})
-				counter = 1;
-			}
+			speed: 2000,
 		});
+
+
+
+
+		//campaignのスライダー用
+		let campaignSlider = new Swiper(".js-campaign-swiper", {
+			loop: true,
+			autoplay: {
+				delay: 2000,
+				disableOnInteraction: false,
+			},
+			speed: 2000,
+			slidesPerView: "auto", // スライドの数を自動調整
+			// 前後の矢印
+			navigation: {
+				nextEl: ".custom-swiper-button-next",
+				prevEl: ".custom-swiper-button-prev",
+			},
+
+			breakpoints: {
+				769: {
+					// 769px以上のデバイスでの設定
+					slidesPerView: "auto", // PCでは3枚半表示
+				},
+			},
+		});
+
+
+
+
+		
+		//画像に色背景がついてから、写真が出てくる
+		//要素の取得とスピードの設定
+		var box = $(".colorbox"),
+			speed = 700;
+
+		//.colorboxの付いた全ての要素に対して下記の処理を行う
+		box.each(function () {
+			$(this).append('<div class="color"></div>');
+			var color = $(this).find($(".color")),
+				image = $(this).find("img");
+			var counter = 0;
+			image.css('opacity', '0');
+			color.css('width', '0%');
+
+			//inviewを使って背景色が画面に現れたら処理をする
+			color.on('inview', function () {
+				if (counter == 0) {
+					$(this).delay(200).animate({ 'width': '100%' }, speed, function () {
+						image.css('opacity', '1');
+						$(this).css({ 'left': '0', 'right': 'auto' });
+						$(this).animate({ 'width': '0%' }, speed);
+					})
+					counter = 1;
+				}
+			});
+		});
+
 	});
-
-
-
-
-
 });
